94f930687499d8472168bb5b703d1069808d0b77
unknown <kevin133557@gmail.com>
2024-09-17 02:16:56 -0400

    Mensaje de confirmación

--- src/app/service/websocket.service.ts
+++ src/app/service/websocket.service.ts
@@ -1,30 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 import { BehaviorSubject } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class WebsocketService {
   private socketStatus = new BehaviorSubject<boolean>(false);
-  constructor(public Socket: Socket) {
+
+  constructor(public socket: Socket) {  // Cambia 'Socket' por 'socket'
     this.checkStatus();
   }
+
   checkStatus() {
-    this.Socket.on('connect', () => {
+    this.socket.on('connect', () => {
       console.log('Cliente conectado');
       this.socketStatus.next(true);
     });
-    this.Socket.on('disconnect', () => {
-      console.log('Cliente conectado');
+
+    this.socket.on('disconnect', () => {
+      console.log('Cliente desconectado');
       this.socketStatus.next(false);
     });
   }
+
   emit(evento: string, payload?: any, callback?: Function) {
-    this.Socket.emit(evento, payload, callback);
+    this.socket.emit(evento, payload, callback);
   }
+
   listen(evento: string) {
-    return this.Socket.fromEvent(evento);
+    return this.socket.fromEvent(evento);
   }
+
   getStatus() {
     return this.socketStatus.asObservable();
   }
